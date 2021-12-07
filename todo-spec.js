59de8603972590a8b56772103ee68a58c98285c4
Wassim <benmesswass@gmail.com>
2021-12-07 10:12:32 +0100

    spec updated

--- todo-spec.js
+++ todo-spec.js
@@ -1,3 +1,4 @@
+/*
 describe('angularjs homepage', function() {
     it('should greet the named user', function() {
       browser.get('http://www.angularjs.org');
@@ -6,4 +7,38 @@ describe('angularjs homepage', function() {
       expect(greeting.getText()).toEqual('Hello Julie!');
     });
   });
+  */
+
+  describe('Protractor baby steps',function() {  
+
+
+    it('Open Angular js website',function() {
+        
+      browser.get("https://angularjs.org");
+      console.log("I am the first step to execute");
+
+      browser.get("https://juliemr.github.io/protractor-demo/").then(function()
+      {
+        console.log("I am the last step to execute");
+
+      })
+
+      console.log("I am the second step to execute");
+    
+    
+    })
+    
+     
+    
+    
+    //Each it block will be called as a spec
+    
+    
+        it('close browswer', function() {
+    
+    })
+    
+    
+    
+    })
   
